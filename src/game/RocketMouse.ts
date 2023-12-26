288cc6a790e38ff94459a88f6f263efed56ba467
a14main <105232109+a14main@users.noreply.github.com>
2023-12-26 11:50:31 +0000

    Kill mouse

--- src/game/RocketMouse.ts
+++ src/game/RocketMouse.ts
@@ -3,8 +3,15 @@ import Phaser from "phaser";
 import TextureKeys from "../consts/TextureKeys";
 import AnimationKeys from "../consts/AnimationKeys";
 
+enum MouseState {
+    Running,
+    Killed,
+    Dead
+}
+
 export default class RocketMouse extends Phaser.GameObjects.Container {
 
+    private mouseState = MouseState.Running;
     private mouse!: Phaser.GameObjects.Sprite
     private flames!: Phaser.GameObjects.Sprite
     private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
@@ -12,7 +19,7 @@ export default class RocketMouse extends Phaser.GameObjects.Container {
 
     constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, x, y);
-        
+
         this.cursors = scene.input.keyboard.createCursorKeys();
         this.pointer = scene.input.pointer1
 
@@ -23,17 +30,17 @@ export default class RocketMouse extends Phaser.GameObjects.Container {
         )
             .setOrigin(0.5, 1)
             .play(AnimationKeys.RocketMouseRun);
-        
+
         this.flames = scene.add.sprite(
             -63,
             -15,
             TextureKeys.RocketMouse
         )
             .play(AnimationKeys.RocketFlamesOn);
-        
+
         this.add(this.flames);
         this.add(this.mouse);
-        
+
         scene.physics.add.existing(this);
 
         const body = this.body as Phaser.Physics.Arcade.Body
@@ -46,20 +53,33 @@ export default class RocketMouse extends Phaser.GameObjects.Container {
     preUpdate() {
         const body = this.body as Phaser.Physics.Arcade.Body;
 
-        if (this.cursors.space?.isDown || this.pointer.isDown) {
-            body.setAccelerationY(-600);
-            this.enableJetpack(true);
-            this.mouse.play(AnimationKeys.RocketMouseFly, true);
-        } else {
-            body.setAccelerationY(0);
-            this.enableJetpack(false);
-        }
-        if (body.blocked.down) {
-            this.mouse.play(AnimationKeys.RocketMouseRun, true);
-        } else if (body.velocity.y > 0) {
-            this.mouse.play(AnimationKeys.RocketMouseFall, true);
-        }
+        switch (this.mouseState) {
+            case MouseState.Running:
 
+                if (this.cursors.space?.isDown || this.pointer.isDown) {
+                    body.setAccelerationY(-600);
+                    this.enableJetpack(true);
+                    this.mouse.play(AnimationKeys.RocketMouseFly, true);
+                } else {
+                    body.setAccelerationY(0);
+                    this.enableJetpack(false);
+                }
+                if (body.blocked.down) {
+                    this.mouse.play(AnimationKeys.RocketMouseRun, true);
+                } else if (body.velocity.y > 0) {
+                    this.mouse.play(AnimationKeys.RocketMouseFall, true);
+                }
+                break;
+            case MouseState.Killed:
+                body.velocity.x *= 0.99;
+                if (body.velocity.x <= 5) {
+                    this.mouseState = MouseState.Dead;
+                }
+                break;
+            case MouseState.Dead:
+                body.setVelocity(0,0);
+                break;
+        }
 
     }
 
@@ -68,6 +88,13 @@ export default class RocketMouse extends Phaser.GameObjects.Container {
     }
 
     kill() {
+
+        if (this.mouseState !== MouseState.Running) {
+            return
+        }
+
+        this.mouseState = MouseState.Killed;
+
         this.mouse.play(AnimationKeys.RocketMouseDead);
 
         const body = this.body as Phaser.Physics.Arcade.Body
